b02243277b0f6a4e915d60b6afacb14c7678d331
Muhammad Yamin <muhammadyamin1081@gmail.com>
2024-08-12 10:30:52 +0700

    Mengembalikan Alert Parameter Model

--- parameterModel.php
+++ parameterModel.php
@@ -128,6 +128,25 @@ $conn->close();
 
         <div class="content mt-3">
 
+            <div class="col-lg-12">
+                <?php if (isset($_SESSION['error'])) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                        ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if (isset($_SESSION['success'])) : ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                        ?>
+                    </div>
+                <?php endif; ?>
+            </div>
             <div class="col-lg-8">
                 <div class="sufee-alert alert with-close alert-danger alert-dismissible fade show" role="alert" style="text-align: justify;">
                     <i class="fa fa-exclamation-triangle"></i>
